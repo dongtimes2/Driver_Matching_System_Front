db61d81618f1382254f7c7ca457cdc15c0585754
dongtimes2 <dongtimes2@gmail.com>
2023-07-26 19:44:49 +0900

    feat: styled-component 전역 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import GlobalStyle from "./GlobalStyle";
+import { theme } from "./styles/theme";
 
 const root = createRoot(document.getElementById("root") as Element);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+);
