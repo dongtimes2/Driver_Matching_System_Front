ec412ffad02d74c6d0d3a1e2a29300e7654acd33
dongtimes2 <dongtimes2@gmail.com>
2023-07-27 03:36:47 +0900

    feat: 유저 유형에 대한 상수 데이터 구현

---
+++ src/constants/user.ts
@@ -0,0 +1,8 @@
+import { IUserTypeList } from "../types/accounts";
+
+export const temp = {};
+
+export const USER_TYPE_LIST: IUserTypeList[] = [
+  { type: "driver", name: "기사회원" },
+  { type: "passenger", name: "승객회원" },
+];
