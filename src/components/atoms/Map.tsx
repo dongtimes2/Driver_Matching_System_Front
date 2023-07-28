87c653e15f0f92b22f3e0e767f60f4c47ef3b3f8
dongtimes2 <dongtimes2@gmail.com>
2023-07-28 18:05:37 +0900

    feat: 컴포넌트를 통해 동적으로 script 태그 생성하도록 변경, 요청사항에 맞는 지도 기능 추가 구현

--- src/components/atoms/Map.tsx
+++ src/components/atoms/Map.tsx
@@ -1,15 +1,19 @@
+/* eslint-disable */
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { BOTTOM_PADDING, TOP_PADDING } from "../../constants/layout";
+import { DEFAULT_LATITUDE, DEFAULT_LONGITUDE } from "../../constants/map";
+import { ICoordinate } from "../../types/map";
 
 declare global {
   interface Window {
-    // eslint-disable-next-line
     kakao: any;
   }
 }
 
-const { kakao } = window;
+interface Props {
+  setCoordinate: React.Dispatch<React.SetStateAction<ICoordinate>>;
+}
 
 const Wrapper = styled.div`
   width: 100%;
@@ -21,17 +25,58 @@ const Wrapper = styled.div`
   }
 `;
 
-function Map() {
-  const map = useRef<unknown>(null);
-
+function Map({ setCoordinate }: Props) {
   useEffect(() => {
-    const container = document.getElementById("map");
-    const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
-      level: 3, // 지도의 레벨(확대, 축소 정도)
-    };
+    const mapScript = document.createElement("script");
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.JAVASCRIPT_KEY}&autoload=false`;
+    document.head.appendChild(mapScript);
 
-    map.current = new window.kakao.maps.Map(container, options);
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        let latitude = DEFAULT_LATITUDE;
+        let longitude = DEFAULT_LONGITUDE;
+
+        const onSuccess = (position: any) => {
+          latitude = position.coords.latitude;
+          longitude = position.coords.longitude;
+          createMap(latitude, longitude);
+        };
+
+        const onError = () => {
+          createMap(latitude, longitude);
+        };
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        } else {
+          onError();
+        }
+
+        const createMap = (latitude: number, longitude: number) => {
+          const options = {
+            center: new window.kakao.maps.LatLng(latitude, longitude),
+            level: 3, // 지도의 레벨(확대, 축소 정도)
+          };
+          const map = new window.kakao.maps.Map(container, options);
+          const marker = new window.kakao.maps.Marker({
+            position: map.getCenter(),
+          });
+          marker.setMap(map);
+          window.kakao.maps.event.addListener(
+            map,
+            "click",
+            (mouseEvent: any) => {
+              const latlng = mouseEvent.latLng;
+              marker.setPosition(latlng);
+              setCoordinate({ latitude: latlng.La, longitude: latlng.Ma });
+            }
+          );
+        };
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
   }, []);
 
   return (
