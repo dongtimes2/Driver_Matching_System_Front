906f927b83be8f183f61d47198dd0cd0b54dc48d
dongtimes2 <dongtimes2@gmail.com>
2023-08-03 04:05:52 +0900

    feat: 유저의 타입을 골라 회원 정보를 갱신할 있게 하는 organism 구현

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -1,8 +1,8 @@
-import { IUserTypeList } from "../types/accounts";
+import { IUser } from "../types/accounts";
 
 export const temp = {};
 
-export const USER_TYPE_LIST: IUserTypeList[] = [
+export const USER_TYPE_LIST: IUser[] = [
   { type: "driver", name: "기사회원" },
   { type: "passenger", name: "승객회원" },
 ];
