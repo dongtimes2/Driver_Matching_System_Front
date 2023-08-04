dff5f1020163764d10137b65ca6f1aa38e6c8504
dongtimes2 <dongtimes2@gmail.com>
2023-08-05 02:24:31 +0900

    feat: 지도 페이지에 이름 표시가 뜨는 헤더가 뜨도록 구현

--- src/pages/Driver.tsx
+++ src/pages/Driver.tsx
@@ -1,15 +1,15 @@
-// import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Layout from "../styles/Layout";
-// import Header from "../components/molecule/Header";
+import Header from "../components/molecule/Header";
 import DriverService from "../components/organism/DriverService";
 
 function Driver() {
-  // const location = useLocation();
-  // const { name }: { name: string } = location.state;
+  const location = useLocation();
+  const { name }: { name: string } = location.state;
 
   return (
     <Layout>
-      {/* <Header userName={name} showRightArea /> */}
+      <Header userName={name} showRightArea />
       <DriverService />
     </Layout>
   );
--- src/pages/Passenger.tsx
+++ src/pages/Passenger.tsx
@@ -1,15 +1,15 @@
-// import { useLocation } from "react-router-dom";
-// import Header from "../components/molecule/Header";
+import { useLocation } from "react-router-dom";
+import Header from "../components/molecule/Header";
 import Layout from "../styles/Layout";
 import PassengerService from "../components/organism/PassengerService";
 
 function Passenger() {
-  // const location = useLocation();
-  // const { name }: { name: string } = location.state;
+  const location = useLocation();
+  const { name }: { name: string } = location.state;
 
   return (
     <Layout>
-      {/* <Header userName={name} showRightArea /> */}
+      <Header userName={name} showRightArea />
       <PassengerService />
     </Layout>
   );
