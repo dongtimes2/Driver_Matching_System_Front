12db504417e8a54c66c67be75255f0c70bcf5b07
dongtimes2 <dongtimes2@gmail.com>
2023-08-07 18:50:41 +0900

    feat: 로그인 여부를 전역으로 관리하도록 recoil atom 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import GlobalStyle from "./GlobalStyle";
 import { theme } from "./styles/theme";
 
 const root = createRoot(document.getElementById("root") as Element);
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
 );
