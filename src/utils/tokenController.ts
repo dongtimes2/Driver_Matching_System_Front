3b93bdbcff97ddd9c5dd228a1b193c27110947d7
dongtimes2 <dongtimes2@gmail.com>
2023-08-07 18:43:45 +0900

    feat: refresh token 로직 제거 및 token 만료 로직 추가

--- src/utils/tokenController.ts
+++ src/utils/tokenController.ts
@@ -1,31 +1,39 @@
 export class TokenController {
   ACCESS_TOKEN_KEY: string;
 
-  REFRESH_TOKEN_KEY: string;
+  TOKEN_EXPIRY_TIME_KEY: string;
 
   constructor() {
     this.ACCESS_TOKEN_KEY = "access_token_key";
-    this.REFRESH_TOKEN_KEY = "refresh_token_key";
+    this.TOKEN_EXPIRY_TIME_KEY = "token_expiry_time_key";
   }
 
   getAccessToken() {
     return localStorage.getItem(this.ACCESS_TOKEN_KEY);
   }
 
-  getRefreshToken() {
-    return localStorage.getItem(this.REFRESH_TOKEN_KEY);
+  getTokenExpiryTime() {
+    return localStorage.getItem(this.TOKEN_EXPIRY_TIME_KEY);
   }
 
   setAccessToken(accessToken: string) {
     localStorage.setItem(this.ACCESS_TOKEN_KEY, accessToken);
   }
 
-  setRefreshToken(refreshToken: string) {
-    localStorage.setItem(this.REFRESH_TOKEN_KEY, refreshToken);
+  setTokenExpiryTime() {
+    const oneHourLater = new Date()
+      .setHours(new Date().getHours() + 1)
+      .toString();
+    localStorage.setItem(this.TOKEN_EXPIRY_TIME_KEY, oneHourLater);
+  }
+
+  isTokenExpired() {
+    const time = this.getTokenExpiryTime();
+    return !time || time < new Date().getTime().toString();
   }
 
   clear() {
     localStorage.removeItem(this.ACCESS_TOKEN_KEY);
-    localStorage.removeItem(this.REFRESH_TOKEN_KEY);
+    localStorage.removeItem(this.TOKEN_EXPIRY_TIME_KEY);
   }
 }
