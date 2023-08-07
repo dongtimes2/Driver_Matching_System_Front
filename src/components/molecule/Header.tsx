dc1cfac9ab80e13ca5b38db32d99e10c71fa1249
dongtimes2 <dongtimes2@gmail.com>
2023-08-07 18:53:51 +0900

    feat: 헤더에 로고 표시 및 로그아웃 버튼 추가

--- src/components/molecule/Header.tsx
+++ src/components/molecule/Header.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import HeaderButton from "../atoms/HeaderButton";
 import { HEADER_HEIGHT, SIDE_PADDING } from "../../constants/layout";
+import signoutIcon from "../../assets/img/signout.png";
+import logoIcon from "../../assets/img/logo.png";
+import useAuth from "../../hooks/useAuth";
 
 interface Props {
   userName?: string;
@@ -21,33 +24,62 @@ const Wrapper = styled.header`
   border-bottom: 2px solid ${({ theme }) => theme.colors.grayLight};
 
   .leftArea {
+    flex-basis: 30%;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
     width: 100%;
-    flex-basis: 10%;
+    color: ${({ theme }) => theme.colors.grayDark};
+    font-size: ${({ theme }) => theme.fontSize.subTitle};
+    font-weight: ${({ theme }) => theme.fontWeight.heavy};
+
+    img {
+      width: 2.5rem;
+      height: 2.5rem;
+    }
+
+    p {
+      transform: translateY(10%);
+    }
   }
 
   .rightArea {
-    flex-basis: 80%;
+    flex-basis: 70%;
     display: flex;
     justify-content: flex-end;
     align-items: center;
-    gap: 1.5rem;
+    gap: 0.75rem;
+    color: ${({ theme }) => theme.colors.grayDark};
+    font-size: ${({ theme }) => theme.fontSize.lg};
+    font-weight: ${({ theme }) => theme.fontWeight.heavy};
+
+    p {
+      transform: translateY(17%);
+    }
   }
 `;
 
 function Header({ userName, showRightArea }: Props) {
+  const { signout } = useAuth();
+
+  const handleSignoutButtonClick = async () => {
+    await signout();
+  };
+
   return (
     <Wrapper>
       <div className="leftArea">
-        <span>LOGO</span>
+        <img src={logoIcon} alt="logo" />
+        <p>drinder</p>
       </div>
-      <div className="rightArea">
-        {showRightArea && (
-          <>
-            <p>{userName ?? "사용자"}님</p>
-            <HeaderButton onClick={() => null}>버</HeaderButton>
-          </>
-        )}
-      </div>
+      {showRightArea && (
+        <div className="rightArea">
+          <p>{userName ?? "사용자"} 님</p>
+          <HeaderButton onClick={handleSignoutButtonClick}>
+            <img src={signoutIcon} alt="signoutIcon" />
+          </HeaderButton>
+        </div>
+      )}
     </Wrapper>
   );
 }
